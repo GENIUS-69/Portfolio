c19336260398e56a313972558689b7e41864b63d
GENIUS-69 <galaxygaming0511@gmail.com>
2026-05-04 19:26:58 +0530

    resume updation

--- components/intro.tsx
+++ components/intro.tsx
@@ -72,7 +72,7 @@ export default function Intro() {
 
         <Link
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="https://drive.google.com/file/d/1eHju3bE0TbLiE-_gSQwiAxDkJjRKEdik/view"
+          href="https://drive.google.com/file/d/12-tMFsUOnOBqdAo2sEqZVYO9i6iUK6NE/view"
           download
         >
           Download CV{" "}
